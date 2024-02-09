d99a4ccd3517081205c561dbf407783da32fb3d3
Stefan Puchmann <sp@timeular.com>
2024-02-09 12:45:21 +0100

    Small CLI improvements

--- internal/cmd/generate.go
+++ internal/cmd/generate.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/sgeisbacher/container-juggler/internal/generation"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var generateCmd = &cobra.Command{
@@ -15,6 +17,25 @@ var generateCmd = &cobra.Command{
 	Long: `TODO: longer description
 
 [scenario] defaults to all`,
+	Args: cobra.RangeArgs(0, 1),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		scenarios := viper.GetStringMapStringSlice("scenarios")
+		var res []string
+		for key := range scenarios {
+			if len(toComplete) != 0 {
+				if strings.HasPrefix(key, toComplete) {
+					res = append(res, key)
+				}
+			} else {
+				res = append(res, "blub")
+			}
+		}
+		return res, cobra.ShellCompDirectiveNoFileComp
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("running generate command ...")
 		generator := generation.CreateGenerator()
